e5e5e08a2c34447131e8ba197e04d6e9f4ffd6d3
Moose Davis <moosedavis2011@gmail.com>
2020-09-14 14:57:17 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,8 +1,7 @@
 def my_find(collection)
-  i = 0 
-  while i < collection.length 
-    return collection[i] if 
-yield(colletion[i])
-    i = i + 1 
-  end 
+  i = 0
+  while i < collection.length
+    return collection[i] if yield(collection[i])
+    i = i + 1
+  end
 end
